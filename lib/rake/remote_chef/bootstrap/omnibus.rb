b6336ab67c5600b648da649f30e1949eb224cf07
yuanying <yuanying@fraction.jp>
2013-07-10 19:16:02 +0900

    Bug gixed.

--- lib/rake/remote_chef/bootstrap/omnibus.rb
+++ lib/rake/remote_chef/bootstrap/omnibus.rb
@@ -3,6 +3,8 @@ set :remote_chef_path, '/usr'
 
 namespace :chef do
   namespace :bootstrap do
+  end
+
   desc 'Install chef.'
   remote_task :bootstrap do
     sudo "curl -L http://www.opscode.com/chef/install.sh | bash"
